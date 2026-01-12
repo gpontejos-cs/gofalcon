7877103dd512db664cf530f7e36ff6b6f20be6f8
Greg Pontejos <242696964+gpontejos-cs@users.noreply.github.com>
2026-01-12 10:38:01 -0600

    update naming

--- falcon/client/cloud_policies/update_suppression_rule_parameters.go
+++ falcon/client/cloud_policies/update_suppression_rule_parameters.go
@@ -15,7 +15,7 @@ import (
 	cr "github.com/go-openapi/runtime/client"
 	"github.com/go-openapi/strfmt"
 
-	"github.com/crowdstrike/gofalcon/falcon-output/models"
+	"github.com/crowdstrike/gofalcon/falcon/models"
 )
 
 // NewUpdateSuppressionRuleParams creates a new UpdateSuppressionRuleParams object,
